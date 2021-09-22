d8d5ea447a8d0258e7a77ec7102107c1cffa179c
mineo72 <mineo72inmc@gmail.com>
2021-09-22 10:46:05 -0400

    JS beginnings

--- js/tr.js
+++ js/tr.js
@@ -1,5 +1,26 @@
 let submitButton = document.querySelector('#bttn');
 function clickListener(event){
+    document.getElementById('error').innerHTML = '';
+    document.getElementById('success').innerHTML = ''
+    let errorMessage = "You're missing the following fields:";
+    let error = 0
 
+    let fnValue = document.getElementById('fn').value;
+    if(Value == ""){
+        errorMessage += "<br>&nbsp&nbsp&middot;First Name";
+        error = 1};
+
+
+    /*let Value = document.getElementById('').value;
+    if(Value == ""){
+        errorMessage += "<br>&nbsp&nbsp&middot;";
+        error = 1};*/
+    
+    if(error == 1){
+        document.getElementById('error').innerHTML = errorMessage
+    } else{
+        document.getElementById('success').innerHTML = 'All Good!'
+    };
+        
 }
 submitButton.addEventListener('click',clickListener)
