2bb51727d975da8a4e58494c5a8012ef829901d9
mineo72 <mineo72inmc@gmail.com>
2021-09-23 09:44:46 -0400

    have to save stuff

--- js/tr.js
+++ js/tr.js
@@ -1,3 +1,5 @@
+
+
 let submitButton = document.querySelector('#button');
 function clickListener(event){
     document.getElementById('error').innerHTML = '';
@@ -6,21 +8,37 @@ function clickListener(event){
     let error = 0
 
     let fnValue = document.getElementById('fn').value;
-    if(Value == ""){
+    if(fnValue == "" || fnValue.length < 2){
         errorMessage += "<br>&nbsp&nbsp&middot;First Name";
         error = 1};
 
 
-    /*let Value = document.getElementById('').value;
-    if(Value == ""){
-        errorMessage += "<br>&nbsp&nbsp&middot;";
-        error = 1};*/
+    let lnValue = document.getElementById('ln').value;
+    if(lnValue == ""){
+        errorMessage += "<br>&nbsp&nbsp&middot;Last Name";
+        error = 1};
+
+    let ccValue = document.getElementById('cc').value;
+    if(ccValue == "" || ccValue.length < 16){
+        errorMessage += "<br>&nbsp&nbsp&middot;Credit Card";
+        error = 1};
     
+    let cvvValue = document.getElementById('cvv').value;
+    if(ccvValue.length != 3 || typeof cvvValue != Number){
+        errorMessage += "<br>&nbsp&nbsp&middot;CVV";
+        error = 1};
+
+
+
+
+
     if(error == 1){
         document.getElementById('error').innerHTML = errorMessage
     } else{
         document.getElementById('success').innerHTML = 'All Good!'
     };
-        
+       
+    const d = new Date()
+    document.getElementById('date').innerHTML = d; 
 }
 submitButton.addEventListener('click',clickListener)
