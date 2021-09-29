82dfa56b6b599a07ebd118f997c20fc002316468
Harper <23HarperM@mydacc.org>
2021-09-29 09:16:31 -0400

    Account Update

--- js/accountsigninjs.js
+++ js/accountsigninjs.js
@@ -1,10 +1,11 @@
+//ignore this javascript, it doesn't work quite yet.
+/*
 let submitButton = document.querySelector(`#submit`);
 
 function clickListener(event){
     //Create error message
     let errorMessage = "Please enter a valid:";
     let error = 0;
-
 
     //Email
     let emailInput = document.getElementById(`email`);
@@ -15,27 +16,24 @@ function clickListener(event){
         error = 1;
     }
 
-
     //Password
     let pswInput = document.getElementById(`psw`);
     let pswValue = pswInput.value;
     //Validate password
-    if(emailValue == "" || !emailValue.includes('@')){
-        errorMessage += "<br>&nbsp;&nbsp;&middot;Email Address";
+    if(pswValue == ""){
+        errorMessage += "<br>&nbsp;&nbsp;&middot;Password";
         error = 1;
     }
     
-
-    //Correct input message
-    let correctResponse = "Hello "+nameValue+". All information has been entered correctly. Thank you.";
 
 
     if(error == 1){
         document.getElementById(`error`).innerHTML=errorMessage;
     }else{
-        document.getElementById(`correct`).innerHTML=correctResponse;
+        location.href = "account.html";
     } 
 
 }
 //Add event listener to listen for a click, then call the function.
 submitButton.addEventListener('click',clickListener);
+*/
