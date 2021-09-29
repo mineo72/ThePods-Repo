c797e21d92f5b91ab5a435b24f7d02b8028f8c82
mineo72 <mineo72inmc@gmail.com>
2021-09-29 09:40:33 -0400

    Fix Account sign in (Micah Olson)

--- js/accountsigninjs.js
+++ js/accountsigninjs.js
@@ -1,5 +1,5 @@
 //ignore this javascript, it doesn't work quite yet.
-/*
+
 let submitButton = document.querySelector(`#submit`);
 
 function clickListener(event){
@@ -7,33 +7,24 @@ function clickListener(event){
     let errorMessage = "Please enter a valid:";
     let error = 0;
 
-    //Email
-    let emailInput = document.getElementById(`email`);
-    let emailValue = emailInput.value;
-    //Validate email
+    let emailValue = document.getElementById('email').value;
     if(emailValue == "" || !emailValue.includes('@')){
-        errorMessage += "<br>&nbsp;&nbsp;&middot;Email Address";
-        error = 1;
-    }
+        errorMessage += "<br>&nbsp&nbsp&middot;Email";
+        error = 1}; 
 
-    //Password
-    let pswInput = document.getElementById(`psw`);
-    let pswValue = pswInput.value;
-    //Validate password
+    let pswValue = document.getElementById('psw').value;
     if(pswValue == ""){
-        errorMessage += "<br>&nbsp;&nbsp;&middot;Password";
-        error = 1;
-    }
+        errorMessage += "<br>&nbsp&nbsp&middot;Password";
+        error = 1}; 
     
 
 
     if(error == 1){
         document.getElementById(`error`).innerHTML=errorMessage;
-    }else{
-        location.href = "account.html";
+    } else{
+        window.location.href = "account.html";
     } 
 
 }
 //Add event listener to listen for a click, then call the function.
 submitButton.addEventListener('click',clickListener);
-*/
